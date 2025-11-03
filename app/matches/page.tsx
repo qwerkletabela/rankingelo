958c38d5ef10ed315101c5f799f13b234a94ab9b
qwerkletabela <qwerkle.tabela@gmail.com>
2025-11-03 13:50:01 +0100

    Replace HeroGlobal component with Hero

--- app/matches/page.tsx
+++ app/matches/page.tsx
@@ -1,12 +1,12 @@
 import NavBar from "@/components/NavBar";
 import RecentMatchesTable from "@/components/RecentMatchesTable";
-import HeroGlobal from "@/components/HeroGlobal";
+import Hero from "@/components/Hero";
 
 export default function MatchesPage() {
   return (
     <main>
       <NavBar />
-      <HeroGlobal />
+      <Hero />
       <div className="max-w-6xl mx-auto px-4 py-6 grid gap-6">
         <RecentMatchesTable />
       </div>
