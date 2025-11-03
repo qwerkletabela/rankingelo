3274cb5908b3e6fb5ee65748336aaf74707a5218
qwerkletabela <qwerkle.tabela@gmail.com>
2025-11-03 21:32:35 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true },
+  experimental: { typedRoutes: false },
 
   // jeśli chcesz tymczasowo wyłączyć ESLint w buildzie, ustaw true:
   eslint: { ignoreDuringBuilds: true },
