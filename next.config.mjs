59e066f30be06de6ed7ac21d8ca00e952bad8e7d
qwerkletabela <qwerkle.tabela@gmail.com>
2025-11-03 11:40:31 +0100

    Configure webpack alias in next.config.mjs
    Added webpack alias configuration for project path.

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,21 @@
+import path from "path";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),   // ← alias @ wskazuje na katalog projektu
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
