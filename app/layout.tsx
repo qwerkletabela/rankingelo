1d6166bba3a9155ccb4c6ad3868c757a1f1f617c
qwerkletabela <qwerkle.tabela@gmail.com>
2025-11-03 10:37:04 +0100

    Import Hero component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Hero from "@/components/Hero";
 import HeroGlobal from "@/components/HeroGlobal";
 
 const inter = Inter({ subsets: ["latin"] });
