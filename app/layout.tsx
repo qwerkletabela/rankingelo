f714bd10520714890dc9fa895289e9ee9c04769d
qwerkletabela <qwerkle.tabela@gmail.com>
2025-11-06 12:05:40 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pl">
-      <body className={`${inter.className} bg-rose-50 min-h-screen`}>
+      <body className={`${inter.className} bg-rose-100 min-h-screen`}>
         <NavBar />
         <div className="pt-14 md:pt-16">
           {children}
